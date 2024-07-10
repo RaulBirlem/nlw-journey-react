c0dd1e67fc9b830ca89ade051ef781dc454a1642
RaulBirlem <raul.birlem@gmail.com>
2024-07-10 17:54:57 -0300

    First commit

---
+++ frontend/src/app.tsx
@@ -0,0 +1,26 @@
+
+
+export function App() {
+
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6 text-center">
+        <p className="zinc-300 text-lg">
+          Convide seus amigos e planeje sua próxima viagem!
+        </p>
+
+        <div>
+
+        </div>
+
+        <p className="text-sm text-zinc-500">
+        Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+        com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
+        </p>
+
+      </div>
+    </div>
+  )
+}
+
